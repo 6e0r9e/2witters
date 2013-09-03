c417a3bcf1d01ec3db2b39277bbd2509e079b9e0
6e0r9e <1@6e0r9e.com>
2013-09-03 15:20:31 -0400

    Add variable to spec to refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  let(:base_title) {"2witters"}
   describe "Home page" do
     it "Should have the content '2witters App'" do
       visit '/static_pages/home'
@@ -8,14 +9,14 @@ describe "Static Pages" do
     end
     it "Should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe "Help page" do
     it "Should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
     it "Should have the content 'Help'" do
       visit '/static_pages/help'
@@ -26,7 +27,7 @@ describe "Static Pages" do
   describe "About page" do
     it "Should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title} | About")
     end
     it "Should have the content 'About'" do
       visit '/static_pages/about'
@@ -37,7 +38,7 @@ describe "Static Pages" do
   describe "Contact page" do
     it "Should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
     it "Should have the content 'Contact'" do
       visit '/static_pages/contact'
