b7831d2828d5f624f5af10cba877ef2b8f79b8a6
6e0r9e <1@6e0r9e.com>
2013-09-12 17:18:57 -0400

    Add variables to Rspec to refactor

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,13 +5,13 @@ describe "User Pages" do
 
  describe "Signup page" do
   before { visit signup_path}
-  it { should have_selector('h1', :text =>'Sign up')}
+  it { should have_selector('h1', text: 'Sign up')}
   it { should have_title(full_title('Sign up'))}
  end
 
  describe "Sign in" do
    before { visit signin_path }
-   it { should have_selector('h1', :text => 'Sign in')}
+   it { should have_selector('h1', text: 'Sign in')}
    it { should have_title(full_title('Sign in'))}
  end
 end
