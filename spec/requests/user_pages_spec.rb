60c673c5818dc1a4c1af4d5deefb1005bd0934c3
6e0r9e <1@6e0r9e.com>
2013-09-11 22:41:39 -0400

    Remove invalid rails tags and add Sign in with Rspec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,4 +8,10 @@ describe "User Pages" do
   it { should have_content('Sign up')}
   it { should have_title(full_title('Sign up'))}
  end
+
+ describe "Sign in" do
+   before { visit signin_path }
+   it { should have_content('Sign in')}
+   it { should have_title(full_title('Sign in'))}
+ end
 end
