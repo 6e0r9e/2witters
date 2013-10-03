890a1d170c57d023e2239760736d322edd6d0ebf
6e0r9e <1@6e0r9e.com>
2013-10-03 12:30:18 -0400

    Create test for invalid sign credentials

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,5 +7,11 @@ describe "Authentication" do
     before { visit signin_path}
     it {should have_content('Sign in')}
     it {should have_title('Sign in')}
+
+    describe "Invalid sign in credentials" do
+      before {click_button "Sign in"}
+      it {should have_title('Sign in')}
+      it {should have_selector('div.alert.alert-error', text: 'Invalid')}
+    end
   end
 end
