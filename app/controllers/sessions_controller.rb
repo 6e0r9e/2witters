6eae5093d79eadc6252aae91cc77c38fe116f973
6e0r9e <1@6e0r9e.com>
2013-10-03 13:19:03 -0400

    Add flash method in controller for invalid sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-     else
+    else
+      flash[:error] = "Invalid email/password combination"
+      render 'new'
     end
   end
 
