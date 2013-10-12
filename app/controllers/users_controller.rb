e310396398eb27252db21f4c251e00863494d5aa
6e0r9e <1@6e0r9e.com>
2013-10-12 17:36:06 -0400

    Create a correct user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,4 +42,9 @@ class UsersController < ApplicationController
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
   end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
