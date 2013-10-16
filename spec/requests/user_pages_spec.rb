94655b0815a0c8bdb84dd04ec716bd5863a12a1c
6e0r9e <1@6e0r9e.com>
2013-10-16 13:29:59 -0400

    Fixed users edit spec all 85 test are passing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,24 +9,23 @@ describe 'User Pages' do
  end
 
  describe "index" do
-   before do
      let(:user) {FactoryGirl.create(:user)}
      before(:each) do
        sign_in user
        visit users_path
      end
-   end
 
-   it {should have_title('All users')}
-   it {should have_content('All users')}
+     it {should have_title('All users')}
+     it {should have_content('All users')}
 
    describe "pagination" do
      before(:all) {30.times {FactoryGirl.create(:user)}}
      after(:all) {User.delete_all}
+
      it { should have_selector('div.pagination')}
 
      it 'should list each user' do
-       User.all.each do |user|
+       User.paginate(page: 1).each do |user|
         expect(page).to have_selector('li', text: user.name)
        end
      end
