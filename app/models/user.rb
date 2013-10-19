5f93d69f350b66b0a3b9aaeed2fee89c11dd131f
6e0r9e <1@6e0r9e.com>
2013-10-19 13:46:29 -0400

    Add belongs to and has many to user and micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   before_save { self.email = email.downcase}
   before_create :create_remember_token
