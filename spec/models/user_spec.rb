4b3b688b6db3ef8b34837e404195b314d13c7cc3
6e0r9e <1@6e0r9e.com>
2013-10-29 11:57:11 -0700

    Testing for reverse relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,8 @@ describe User do
   it { should respond_to(:feed)}
   it { should respond_to(:relationships)}
   it { should respond_to(:followed_user)}
+  it { should respond_to(:reverse_relationships)}
+  it { should respond_to(:followers)}
   it { should respond_to(:following?)}
   it { should respond_to(:follow!)}
   it { should respond_to(:unfollow!)}
@@ -170,6 +172,11 @@ describe User do
 
       it {should_not be_following(other_user)}
       its(:followed_users) { should_not include(other_user)}
+    end
+
+    describe "followed_user" do
+      subject { other_user }
+      its(:followers) { should include(@user)}
     end
   end
 end
