933c79adb57da91caa9ee2e00c96967ade1b89d0
6e0r9e <1@6e0r9e.com>
2013-10-28 22:02:55 -0700

    Test relationship creation and attributes

--- db/migrate/20131029045444_create_relationships.rb
+++ db/migrate/20131029045444_create_relationships.rb
@@ -6,8 +6,8 @@ class CreateRelationships < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :relationshipss, :follower_id
-    add_index :relationshipss, :followed_id
-    add_index :relationshipss, [:follower_id, :followed_id], unique: true
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
